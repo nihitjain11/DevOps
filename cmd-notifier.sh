35a1ef95dd3019b24a4474e254714238a8749e94
nihitjain11 <nihitjain11@gmail.com>
2019-12-27 17:17:31 +0530

    notification gist

---
+++ cmd-notifier.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# https://gist.github.com/John-Almardeny/04fb95eeb969aa46f031457c7815b07d
+
+# SAMPLE notifications
+# noti "Hello From Noti" "now +1 minute"
+# noti "Hello From Noti" "now +5 minutes"
+# noti "Hello From Noti" "now + 1 hour"
+# noti "Hello From Noti" "now + 2 days"
+# noti "Hello From Noti" "4 PM + 2 days"
+# noti "Hello From Noti" "now + 3 weeks"
+# noti "Hello From Noti" "now + 4 months"
+# noti "Hello From Noti" "4:00 PM"
+# noti "Hello From Noti" "2:30 AM tomorrow"
+# noti "Hello From Noti" "2:30 PM Fri"
+# noti "Hello From Noti" "2:30 PM 25.07.18"
